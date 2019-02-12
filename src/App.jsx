bdc63aad47cb79a9892e55cbb304e1c455ab7865
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-12 02:28:45 +0000

    Modularize elements according to (v loose) instructions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <Message />
+        <ChatBar />
+      </div>
     );
   }
 }
 export default App;
+
