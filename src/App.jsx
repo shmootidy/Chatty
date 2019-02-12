dbaed390e040216c86333b1f739092d2cdad496c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-12 17:27:20 +0000

    Add dummy keys/ids to message list in App, and pass into MessageList child.

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,12 @@ class App extends Component {
         {
           username: "Bob",
           content: "Has anyone seen my marbles?",
+          id: "caw"
         },
         {
           username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: "woof"
         }
       ]
     }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -6,7 +6,7 @@ export default class MessageList extends Component {
 
   const messages = this.props.messages.map((message) => {
     return (
-      <div className="message">
+      <div className="message" key={message.id}>
         <span className="message-username">{message.username}</span>
         <span className="message-content">{message.content}</span>
       </div>
