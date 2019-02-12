e99cb9f25a4db57f48c9e4a2681a1278c491ca6a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-12 16:37:30 +0000

    Reorganize data and props.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ class ChatBar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser} />
+        <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
