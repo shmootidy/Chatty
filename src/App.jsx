dc4bf7f159a6f9fd42123825bdcbf93859563ac8
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-12 05:44:52 +0000

    Create Nav functional component and pass (?) into parent.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import Message from './Message.jsx';
 
+const Nav = () => {
+  return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+    )
+}
+
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar">
-          <a href="/" className="navbar-brand">Chatty</a>
-        </nav>
+        <Nav />
         <Message />
         <ChatBar />
       </div>
