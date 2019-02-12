0febf34beb95853a2b91e447759344ca6719bc99
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-12 17:31:14 +0000

    Move message loop to Message comp.

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,9 +2,17 @@ import React, {Component} from 'react';
 
 export default class Message extends Component {
   render(){
+    const messages = this.props.messages.map((message) => {
+      return (
+        <div className="message" key={message.id}>
+          <span className="message-username">{message.username}</span>
+          <span className="message-content">{message.content}</span>
+        </div>
+      )
+    });
     return (
       <div>
-
+        {messages}
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,18 +4,9 @@ import Message from './Message.jsx';
 export default class MessageList extends Component {
   render() {
 
-  const messages = this.props.messages.map((message) => {
-    return (
-      <div className="message" key={message.id}>
-        <span className="message-username">{message.username}</span>
-        <span className="message-content">{message.content}</span>
-      </div>
-    )
-  });
-
   return (
       <main className="messages">
-        {messages}
+        <Message messages={this.props.messages} />
       </main>
     )
   }
