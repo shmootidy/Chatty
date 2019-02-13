33a7d081ece9774188f1d4718edefc29a78905ac
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-13 20:08:38 +0000

    Remove hard coded messages from App state.

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,7 @@ class App extends Component {
     this.socket = new WebSocket('ws://localhost:3001');
     this.state = {
       currentUser: {name: "Bob"},
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: "caw"
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: "woof"
-        }
-      ]
+      messages: []
     };
     this.handleNewMessage = this.handleNewMessage.bind(this);
   }
