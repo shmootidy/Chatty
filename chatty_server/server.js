006ca347470b31061a0b8c95b3b998a05d978080
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-13 20:20:57 +0000

    Require uuid in server.

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -9,10 +9,13 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+const uuid4 = require('uuid/v4'); // v4 is random, general (I think)
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', (message) => {
     const newMessage = JSON.parse(message);
+    console.log(newMessage);
     console.log(`User ${newMessage.username} said ${newMessage.content}`);
   })
 
