abc90f6b38fa27f3e9280541ea81cde09044e48c
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-14 22:07:03 +0000

    Change another if statement to switch.

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 
 export default class Message extends Component {
   render(){
-    if (this.props.message.type === "incomingMessage") {
-      return (
-        <div className="message">
-          <span className="message-username">{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
-        </div>
-      );
-    } else if (this.props.message.type === "incomingNotification")  {
+    switch(this.props.message.type) {
+      case "incomingMessage":
+        return (
+          <div className="message">
+            <span className="message-username">{this.props.message.username}</span>
+            <span className="message-content">{this.props.message.content}</span>
+          </div>
+        );
+      case "incomingNotification":
       return (
         <div className="notification">
           <span className="notification-content">{this.props.message.content}</span>
